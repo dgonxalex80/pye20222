00c7e0ef7928432f910ac117fcac21c1d0cf8f16
Daniel E Gonzalez <dgonxalex80@gmail.com>
2022-07-01 23:30:17 -0500

    ajustes de menu - reagrupando por modulo

---
+++ site.R
@@ -0,0 +1,150 @@
+name: " "
+output_dir: "."
+navbar:
+  title: " "
+  left:
+    - text: "PyE"
+      icon: fa-home
+      href: index.html
+    - text: "Módulo 0"
+      menu:
+        - text: "Módulo 0"
+          href: Modulo0.html
+        - text: "Recurso 0.1"
+          href: recurso001.html  
+        - text: "Recurso 0.2"
+          href: recurso002.html  
+        - text: "Recurso 0.3"
+          href: recurso003.html  
+        - text: "Recurso 0.4"
+          href: recurso004.html  
+    - text: "Módulo 1"
+      menu:
+        - text: "Módulo 1"
+          href: Modulo1.html
+        - text: "---------"          
+        - text: "Guía 1.1"
+          href: Guia101.html            
+        - text: "Guía 1.2"
+          href: Guia102.html            
+        - text: "Guía 1.3"
+          href: Guia103.html 
+        - text: "---------"  
+        - text: "Recursos 1.1"
+          href: recurso101.html  
+        - text: "Recursos 1.2"
+          href: recurso102.html  
+        - text: "Recurso 1.3"
+          href: recurso103.html
+        - text: "---------"            
+        - text: "Monitoria 1"
+          href: monitoria1.html
+    - text: "Módulo 2"
+      menu:
+        - text: "Módulo 2"
+          href: Modulo2.html
+         - text: "---------"  
+        - text: "Guía 2.1"
+          href: Guia201.html
+        - text: "Guía 2.2"
+          href: Guia202.html             
+        - text: "---------"            
+        - text: "Recursos 2.1"
+          href: recurso201.html  
+        - text: "Recursos 2.2"
+          href: recurso202.html
+        - text: "---------"  
+        - text: "monitoria 2"
+          href: monitoria2.html        
+    - text: "Módulo 3"
+      menu:
+        - text: "Módulo 3"
+          href: Modulo3.html
+        - text: "---------"
+        - text: "Guía 3.1"
+          href: Guia301.html
+        - text: "Guía 3.2"
+          href: Guia302.html
+        - text: "Guía 3.3"
+          href: Guia303.html             
+        - text: "---------"  
+        - text: "Recursos 3.1"
+          href: recursos301.html  
+        - text: "Recursos 3.2"
+          href: recurso302.html  
+        - text: "Recursos 3.3"
+          href: recurso303.html
+        - text: "---------"            
+        - text: "monitoria 3"
+          href: monitoria3.html
+    - text: "Módulo 4"
+      menu:
+        - text: "Módulo 4"
+          href: Modulo4.html
+        - text: "---------"
+        - text: "Guía 4.1"
+          href: Guia401.html
+        - text: "Guía 4.2"
+          href: Guia402.html
+        - text: "Guía 4.3"
+          href: Guia403.html          
+        - text: "---------"          
+        - text: "Recursos 4.1"
+          href: recurso401.html  
+        - text: "Recursos 4.2"
+          href: recurso402.html  
+        - text: "Recursos 4.3"
+          href: recurso403.html
+        - text: "---------"  
+        - text: "monitoria 4"
+          href: monitoria4.html        
+    - text: "Módulo 5"
+      menu:
+        - text: "Módulo 5"
+          href: Modulo5.html
+        - text: "---------"
+        - text: "Guía 5.1"
+          href: Guia501.html
+        - text: "Guía 5.2"
+          href: Guia502.html
+        - text: "---------"          
+        - text: "Recursos 5.1"
+          href: recurso501.html  
+        - text: "Recursos 5.2"
+          href: recurso502.html
+        - text: "---------"            
+        - text: "monitoria 5"
+          href: monitoria5.html
+    - text: "Código R"
+      icon: fa-code
+      menu:
+        - text: "Introducción a R"
+          href: introduccionR.html
+        - text: "Código u1.1"
+          href: codigo101.html
+        - text: "Código u1.2"
+          href: codigo102.html  
+        - text: "Código u1.3"
+          href: codigo103.html
+        - text: "Código u2.1"
+          href: codigo201.html
+        - text: "Código u3.1"
+          href: codigo301.html
+        - text: "Código u3.2"
+          href: codigo302.html
+        - text: "Código u3.3"
+          href: codigo303.html
+        - text: "Código u4.1"
+          href: codigo401.html   
+        - text: "Código u4.2"
+          href: codigo402.html 
+        - text: "Código u4.3"
+          href: codigo403.html
+        - text: "Código u5.1"
+          href: codigo501.html     
+        - text: "Código u5.2"
+          href: codigo502.html           
+output:
+  html_document:
+    theme: flatly
+    css: style.css
