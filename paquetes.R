c2f3206051cbb589e29e72bac6935465aa53738f
Daniel E Gonzalez <dgonxalex80@gmail.com>
2022-07-29 23:00:11 -0500

    ajustes recurso102.Rmd

---
+++ paquetes.R
@@ -0,0 +1,4 @@
+install.packages("learnr") # solo una vez
+install.packages("devtools") # solo una vez
+devtools::install_github("dgonxalex80/paqueteDEG")
+learnr::run_tutorial("Taller-sumatoria", "paqueteDEG")
