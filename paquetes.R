e3abbe78ca690f694cc8c4ef97dd3ac8dad8f68d
Daniel E Gonzalez <dgonxalex80@gmail.com>
2022-10-17 17:34:51 -0500

    add segundo parcial

--- paquetes.R
+++ paquetes.R
@@ -2,3 +2,8 @@ install.packages("learnr") # solo una vez
 install.packages("devtools") # solo una vez
 devtools::install_github("dgonxalex80/paqueteDEG")
 learnr::run_tutorial("Taller-sumatoria", "paqueteDEG")
+
+
+
+install.packages("psych", type = "binary", dependencies = TRUE)
+install.packages("mnormt", type = "binary", dependencies = TRUE)
