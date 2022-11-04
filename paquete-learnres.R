b2285cd575ec4428f8f65ab842124cad6bbad8e7
Daniel E Gonzalez <dgonxalex80@gmail.com>
2022-11-04 07:08:08 -0500

    adicion de taller y resumen de pdeh en pdf

--- paquete-learnres.R
+++ paquete-learnres.R
@@ -1,2 +1,9 @@
+install.packages("learnr")
+install.packages("tidyverse")
+install.packages("palmerpenguins")
+install.packages("devtools")
+devtools::install_github("rundel/learnrhash")
+devtools::install_github("rstudio-education/gradethis")
+
 # install.packages("remotes")
 remotes::install_github("yabellini/learnres")
